c17ac5b8d628849340f6f8055dbd918b20e7d282
bane.shi <fengxiaotx@163.com>
2023-12-25 17:31:02 +0800

    Bane/pom

---
+++ readme.md
@@ -0,0 +1,6 @@
+# 云服务集成
+
+#### 互相吹捧，共同进步
+<div style="width: 100%;text-align: center">
+<img src="images/shuque_wx.jpg" width="200px" alt="">
+</div>
---
+++ triones-csi-huaweicloud/triones-csi-huaweicloud-obs/src/main/java/com/trionesdev/csi/huaweicloud/obs/HuaweiCloudObs.java
@@ -0,0 +1,69 @@
+package com.trionesdev.csi.huaweicloud.obs;
+
+import com.trionesdev.csi.api.oss.OssTemplate;
+import com.trionesdev.csi.api.oss.request.*;
+import com.trionesdev.csi.api.oss.response.OssGetObjectResponse;
+import com.trionesdev.csi.api.oss.response.OssListObjectsResponse;
+import com.trionesdev.csi.api.oss.response.OssPutObjectResponse;
+import com.obs.services.ObsClient;
+import com.obs.services.model.PutObjectRequest;
+import com.obs.services.model.PutObjectResult;
+import com.trionesdev.csi.api.oss.util.OssUtils;
+import org.apache.commons.lang3.StringUtils;
+
+public class HuaweiCloudObs implements OssTemplate {
+    private final ObsClient obsClient;
+    private final HuaweiCloudObsConfig huaweiCloudObsConfig;
+
+    public HuaweiCloudObs(ObsClient obsClient, HuaweiCloudObsConfig huaweiCloudObsConfig) {
+        this.obsClient = obsClient;
+        this.huaweiCloudObsConfig = huaweiCloudObsConfig;
+    }
+
+
+    @Override
+    public OssGetObjectResponse getObject(OssGetObjectRequest request) {
+        return null;
+    }
+
+    @Override
+    public OssPutObjectResponse putObject(OssPutObjectRequest request) {
+        String bucketName = bucketName(request.getBucketName());
+        String urlPrefix = urlPrefix(request.getUrlPrefix());
+        PutObjectRequest putObjectRequest = new PutObjectRequest();
+        putObjectRequest.setBucketName(bucketName);
+        putObjectRequest.setObjectKey(request.getObjectName());
+        putObjectRequest.setInput(request.getInputStream());
+        PutObjectResult result = obsClient.putObject(putObjectRequest);
+        return OssPutObjectResponse.builder().url(OssUtils.joinPrefix(request.getObjectName(), urlPrefix)).build();
+    }
+
+    @Override
+    public void removeObjects(OssRemoveObjectsRequest request) {
+
+    }
+
+    @Override
+    public String getObjectUrl(OssGetObjectUrlRequest request) {
+        return OssUtils.joinPrefix(request.getObjectName(), urlPrefix(request.getUrlPrefix()));
+    }
+
+    @Override
+    public String getObjectName(OssGetObjectNameRequest request) {
+        return OssUtils.removePrefix(request.getObjectUrl(), urlPrefix(request.getUrlPrefix()));
+    }
+
+    @Override
+    public OssListObjectsResponse listObjects(OssListObjectsRequest request) {
+        return null;
+    }
+
+    private String bucketName(String bucketName) {
+        return StringUtils.isNotBlank(bucketName) ? bucketName : huaweiCloudObsConfig.getBucket();
+    }
+
+    private String urlPrefix(String urlPrefix) {
+        return StringUtils.isNotBlank(urlPrefix) ? urlPrefix : huaweiCloudObsConfig.getUrlPrefix();
+    }
+
+}
